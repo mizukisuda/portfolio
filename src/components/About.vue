7c32773999d886b3a26f83844bdf259da7e0227a
mizuki.suda <user@sudanoMacBook-Air.local>
2020-04-13 18:23:49 +0900

    add Head, Main, About, Skill, Vision

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <About />
+  <div id="aboutSection">
+    <div class="aboutTitle">
+      About Me
+    </div>
+    <div class="aboutExplain">
+      <p>
+        北海道出身の新卒一年目。大学において教育学を学ぶなかで、教育の不可能性に気づき、哲学や行動経済学、Webサービスの技術に興味を持つ。以降、エンジニアに方向転換し、未経験から技術を学ぶ。
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +17,33 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* Only comments */
+#aboutSection {
+  background-color: #f5f5f5;
+  height: auto;
+  width: 100%;
+  padding: 0 0 20px 0;
+}
+
+.aboutTitle {
+  padding: 20px 0 20px 0;
+  text-align: center;
+  color: #20879f;
+  font-weight: bold;
+  font-size: 18pt;
+  font-family: "Noto Sans JP", sans-serif;
+  text-shadow: 1px 2px 3px #808080;
+}
+
+.aboutExplain {
+  margin: 0 auto;
+  width: 80%;
+}
+
+p {
+  color: #707070;
+  font-size: 12pt;
+  font-family: "Noto Sans JP", sans-serif;
+  word-wrap: break-all;/* 折り返し */
+  line-height: 0.25 em;/* 行間の調節 */
+}
 </style>
--- src/components/Vision.vue
+++ src/components/Vision.vue
@@ -1,5 +1,14 @@
 <template>
-  <Vision />
+  <div id="visionSection">
+    <div class="visionTitle">
+      Vision
+    </div>
+    <div class="visionExplain">
+      <p>
+        「偶然的な出会い」や「コミュニケーション」を可能とするWebサービスに興味があります。まずはフロントエンドとして開発に関わったのち、行動経済学とUI/UXデザインを学び、UXエンジニアになりたいと考えています。
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +18,30 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 /* Only comments */
+#visionSection {
+  background-color: white;
+  height: auto;
+  width: 100%;
+  padding: 0 0 20px 0;
+}
+
+.visionTitle {
+  padding: 20px 0 20px 0;
+  color: #20879f;
+  font-size: 18pt;
+  font-family: "Noto Sans JP", sans-serif;
+  text-align: center;
+  font-weight: bold;
+  text-shadow: 1px 2px 3px #808080;
+}
+
+.visionExplain {
+  margin: 0 auto;
+  width: 80%;
+  color: #707070;
+  font-size: 12pt;
+  font-family: "Noto Sans JP", sans-serif;
+  word-wrap: break-all;/* 折り返し */
+  line-height: 0.25 em;/* 行間の調節 */
+}
 </style>
